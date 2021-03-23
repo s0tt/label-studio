caffb74a8de596307037ddba62e5b7e473cd7205
Stefan Ott <kicker17@gmx.de>
2021-03-23 23:25:53 +0100

    adaptions cloud run config paths

--- tools/run.sh
+++ tools/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+cd label-studio
 label-studio start ${PROJECT_NAME:-my_project} -b \
              --host ${HOST:-0.0.0.0} --port ${PORT:-8080} --protocol ${PROTOCOL:-http://} \
-             --username ${USERNAME:-""} --password ${PASSWORD:-""} ${INIT_COMMAND:-"--init"}
+             --username ${USERNAME:-""} --password ${PASSWORD:-""}
+#${INIT_COMMAND:-"--init"}
