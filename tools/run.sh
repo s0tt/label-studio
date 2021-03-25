d4a01c5ddcaa901df651b014cec3bf2e210974f9
Stefan Ott <kicker17@gmx.de>
2021-03-25 17:45:03 +0100

    run sh debug info

--- tools/run.sh
+++ tools/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo $PROJECT_NAME
 label-studio start ${PROJECT_NAME:-my_project} -b \
              --host ${HOST:-0.0.0.0} --port ${PORT:-8080} --protocol ${PROTOCOL:-http://} \
              --username ${USERNAME:-""} --password ${PASSWORD:-""}
